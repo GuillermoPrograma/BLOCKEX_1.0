a945c6b189aab8937da8bb73235e4163dbcabfd1
alumno <ayoubbenjaddi5@gmail.com>
2026-01-13 14:15:42 +0100

    La parte de diseño hecho

--- app/src/main/java/com/example/blockex/MainActivity.kt
+++ app/src/main/java/com/example/blockex/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.blockex
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,6 +19,14 @@ class MainActivity : AppCompatActivity() {
             insets
 
         }
+
+            val boton=findViewById<Button>(R.id.ajustes)
+            boton.setOnClickListener {
+                val intento=Intent(this, AjustesActivity::class.java)
+                startActivity(intento)
+            }
+
+
 
     }
 }
