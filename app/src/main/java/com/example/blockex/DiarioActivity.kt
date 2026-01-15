8780de1c8f030055675061216619b232e662c71e
danienrique <danienriquealonso@gmail.com>
2026-01-16 00:47:56 +0100

    Diario commit 16/01/2026 00:47

--- app/src/main/java/com/example/blockex/DiarioActivity.kt
+++ app/src/main/java/com/example/blockex/DiarioActivity.kt
@@ -1,21 +1,30 @@
 package com.example.blockex
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class DiarioActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_diario)
+        setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-            //prueba2
         }
+        val tvFechaHoy = findViewById<TextView>(R.id.tvFechaHoy)
+        val formato = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+        val fechaHoy = formato.format(Date())
+
+        tvFechaHoy.text = fechaHoy
+
     }
 }
