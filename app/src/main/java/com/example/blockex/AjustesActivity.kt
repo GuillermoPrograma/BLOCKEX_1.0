16cdfbabe10f6f9431e2a35cb40b9ca70601a1ec
alumno <ayoubbenjaddi5@gmail.com>
2026-01-20 10:59:25 +0100

    21

--- app/src/main/java/com/example/blockex/AjustesActivity.kt
+++ app/src/main/java/com/example/blockex/AjustesActivity.kt
@@ -1,6 +1,7 @@
 package com.example.blockex
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +17,22 @@ class AjustesActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val botonPrivacidad=findViewById<Button>(R.id.button2)
+        botonPrivacidad.setOnClickListener {
+            mostrarDialogoAvisoLegal()
+
+        }
+    }
+    private fun mostrarDialogoAvisoLegal() {
+        val builder = androidx.appcompat.app.AlertDialog.Builder(this)
+        builder.setTitle("Aviso Legal")
+        builder.setMessage("Para usar esta tienda debes aceptar los términos y condiciones de uso.")
+        builder.setPositiveButton("Aceptar", null)
+        builder.setNegativeButton("Salir") { _, _ ->
+            finish() // Cierra la Activity actual (la app)
+        }
+        builder.setCancelable(false) // Impedimos que se cierre tocando fuera del mensaje
+        builder.show()
     }
 }
